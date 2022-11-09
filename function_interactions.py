2ff56bef9e64d3423afd2b95857f4d36acad66c0
Ezra Mirek <113930317+emirek2@users.noreply.github.com>
2022-11-09 17:58:54 +0000

    dynamic functions

--- function_interactions.py
+++ function_interactions.py
@@ -1,20 +1,28 @@
+import random
+from random import shuffle
 # interaction
 # between functions
 # The outputs of a certain function can become inputs of other
 # functions. That way, the program is built from the interaction
 # between functions that perform a defined task .
 # def function_1():
-# |...
-# |return a
-# def function_2(a):
-# |...
-# |return b
-# def function_3(b):
-# |...
-# |return c
-# def function_4(a,c):
-# |...
-# |return d
+
+
+#stick game
+#intial list
+sticks=['-','--','---','----','-----']
+
+#mixing sticks
+def mix_sticks(my_list):
+  random.shuffle(my_list)
+  return my_list
+
+
+#choose a number
+
+
+
+#verify the number
 
 
 
@@ -24,17 +32,23 @@
 # Create a function (throw_dice) that "throws" two random dice and returns its results (the function MUST RETURN TWO VALUES AS A RESULT, both of which must be between 1 and 6, randomly).
 
 # Pass the result of these two dice to a function called roll_result (meaning that this second function MUST RECEIVE TWO ARGUMENTS) and return -without printing it- a certain message according to the what the sum of these values results:
-
+def throw_dices():
+  dice1=random.randint(1,6)
+  dice2=random.randint(1,6)
+  sum_dice=dice1+dice2
 # If the sum is less than or equal to 6:
-
+  if sum_dice<=6:
+    print(f"The sum of your dice is {sum_dice}. Unfortunate")
 # "The sum of your dice is {sum_dice}. Unfortunate"
-
+  
 # If the sum is greater than 6 and less than 10:
-
+  if sum_dice>6 and sum_dice<10:
+    print(f"The sum of your dice is {sum_dice}. You have a good chance")
 # "The sum of your dice is {sum_dice}. You have a good chance"
 
 # If the sum is greater than or equal to 10:
-
+  if sum_dice>=10:
+    print(f"The sum of your dice is {sum_dice}. It looks like a winning roll")
 # "The sum of your dice is {sum_dice}. It looks like a winning roll"
 
 # Hint: use the random library's choice or randint method to choose a random value between 1 and 6.
--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 from dynamic_functions import check_3Digits, all_positives, sum_less, count_even
+from function_interactions import throw_dices,mix_sticks
 
 
 
+# coffee_prices =[('cappucino', 1.5,),
+#                ('expresso',1.2),
+#                ('mocha',1.9)]
 
-coffee_prices =[('cappucino', 1.5,),
-               ('expresso',1.2),
-               ('mocha',1.9)]
+# #retrieve the names and price from the above tuple
+# for coffee, price in coffee_prices:
+#   print(f"The {coffee} costs ${price}")
 
-#retrieve the names and price from the above tuple
-for coffee, price in coffee_prices:
-  print(f"The {coffee} costs ${price}")
+# #create a function for retriving the highest price coffe and its name
+# def most_expensive(coffee_prices):
+#   highest_prices=0
+#   my_most_expensive=''
+#   for coffe, price in coffee_prices:
+#     if price>highest_prices:
+#       highest_prices = price
+#       my_most_expensive = coffee
+#     else:
+#       pass
+#   return(my_most_expensive, highest_prices)
+# print(most_expensive(coffee_prices))
 
-#create a function for retriving the highest price coffe and its name
-def most_expensive(coffee_prices):
-  highest_prices=0
-  my_most_expensive=''
-  for coffe, price in coffee_prices:
-    if price>highest_prices:
-      highest_prices = price
-      my_most_expensive = coffee
-    else:
-      pass
-  return(my_most_expensive, highest_prices)
-print(most_expensive(coffee_prices))
-
-result=check_3Digits([55,99,600,780,48,120,65,666,37,17,975,17175145])
-print(result)
-end=all_positives([2,3,4,5,-1,5,57,-123])
-print(end)
-sum=sum_less([23,2,2,2,1,1,14,4,14,123,235])
-print(sum)
-counts=count_even([3,3,3,3,3,5,5,2,6,3,4,5,])
-print(counts)
-  
+# result=check_3Digits([55,99,600,780,48,120,65,666,37,17,975,17175145])
+# print(result)
+# end=all_positives([2,3,4,5,-1,5,57,-123])
+# print(end)
+# sum=sum_less([23,2,2,2,1,1,14,4,14,123,235])
+# print(sum)
+# counts=count_even([3,3,3,3,3,5,5,2,6,3,4,5,])
+# print(counts)
+# throw_dices()
+sticks=['-','--','---','----','-----']
+my_mix=mix_sticks(sticks)
+print(my_mix)
   
 
 
